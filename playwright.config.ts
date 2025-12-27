90bccfbf05976594b25bc796fcfdd2e4d87f5bdc
iamSujoy <Sujoy_Nandi@epam.com>
2025-12-27 15:17:51 +0530

    fixed CI issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 2,
+  workers: 2,
   timeout: 30 * 1000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['line'], ['html', { open: 'never' }], ["ortoni-report", reportConfig]],
@@ -21,8 +21,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    headless: false,
+    trace: 'retain-on-failure',
+    headless: !!process.env.CI,
     screenshot: 'only-on-failure',
   },
 
